0d33065ff5c0fe5e27df84835ccf47e4970be722
sadra <sadrataghavi1383@gmail.com>
2022-08-29 14:56:02 +0430

    feat(api)

--- Api/views.py
+++ Api/views.py
@@ -1,4 +1,6 @@
 import json
+import random
+from random import randint
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
@@ -9,8 +11,14 @@ from django.http import HttpResponseRedirect
 
 
 def HeroMovie(request):
+
     response = requests.get(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=92876dd4")
+        f'https://www.omdbapi.com/?t={randomWordGen()}&apikey=92876dd4')
+
+    while response.status_code != 200:
+        response = requests.get(
+            f'https://www.omdbapi.com/?t={randomWordGen()}&apikey=92876dd4')
+
     return JsonResponse(response.json())
 
 
@@ -18,3 +26,11 @@ def AllMovies(request):
     response = requests.get(
         "http://www.omdbapi.com/?i=tt3896198&apikey=92876dd4")
     return JsonResponse(response.json())
+
+
+def randomWordGen() -> str:
+    random_word = random.choice(requests.get(
+        "https://www.mit.edu/~ecprice/wordlist.10000").content.splitlines())
+
+    # transform byte to string
+    return random_word.decode("utf-8")
