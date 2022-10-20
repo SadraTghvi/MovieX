7fe79d405022653d4a6ddc1a6b99823074dbbf81
sadra <sadrataghavi1383@gmail.com>
2022-10-20 17:14:04 +0330

    feat(home): get hero img from backend

--- App/Home/HeroSection.tsx
+++ App/Home/HeroSection.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
+import axios from 'axios'
+
 const HeroSection = () => {
+    axios
+        .get('http://localhost:7000/api/herophoto/', {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+            },
+        })
+        .then(res => console.log(res.data))
+        .catch(res => console.log(res))
     return <section className='hero-section'>HeroSection</section>
 }
 
