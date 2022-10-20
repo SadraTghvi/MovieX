8389e91bfcba81a68d1a8a70811c6012e4a7c611
sadra <sadrataghavi1383@gmail.com>
2022-10-20 13:10:09 +0330

    feat(ablum): models init

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,23 @@
+from turtle import title
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+
+class AlbumCategories(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.category
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    img = models.ImageField()
+    description = models.TextField(
+        max_length=200, default="This Is The Default Description")
+    category = models.ForeignKey(AlbumCategories, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
