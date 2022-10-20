014503938130aa732ca9444bcab71cd2699ade2d
sadra <sadrataghavi1383@gmail.com>
2022-10-20 17:11:57 +0330

    feat(api)

--- Api/views.py
+++ Api/views.py
@@ -1,18 +1,22 @@
-import json
 import random
-from random import randint
 from django.shortcuts import render
 from django.http import JsonResponse
-import requests
 from django.http import HttpResponseRedirect
+from django.core import serializers
+from django.forms.models import model_to_dict
+
+
+from Gallery.models import Album
 
 
 # Create your views here.
 
 
 def HeroPhoto(request):
+    AlbumsPhotos = Album.objects.all()
+    randomAlbumImg = random.choice(AlbumsPhotos)
 
-    return JsonResponse({"status": 200})
+    return JsonResponse({"url": randomAlbumImg.img.url})
 
 
 def AllPhotos(request):
