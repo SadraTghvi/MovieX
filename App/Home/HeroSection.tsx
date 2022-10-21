e85764d8dcadfd4b36ddb6e84d75a82ca2d0240c
sadra <sadrataghavi1383@gmail.com>
2022-10-21 16:55:06 +0330

    fix(hero)

--- App/Home/HeroSection.tsx
+++ App/Home/HeroSection.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import axios from 'axios'
 
 const HeroSection = () => {
-    axios
-        .get('http://localhost:7000/api/herophoto/', {
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-            },
-        })
-        .then(res => console.log(res.data))
-        .catch(res => console.log(res))
-    return <section className='hero-section'>HeroSection</section>
+    const [HeroImg, setHeroImg] = useState<string>('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:7000/api/herophoto/', {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                },
+            })
+            .then(res => setHeroImg(res.data.url))
+            .catch(res => console.log(res))
+    }, [])
+
+    return (
+        <section className='hero-section'>
+            <div
+                className='hero-bg'
+                style={{ backgroundImage: `url(${HeroImg})` }}
+            ></div>
+            <div className='hero-titles'>
+                <div className='title_hero'>Share With</div>
+                <div className='title_small'>
+                    Share Your Purest Moments With Others
+                </div>
+                <div className='title_smaller'>Sign Up Now And Explore</div>
+            </div>
+        </section>
+    )
 }
 
 export default HeroSection
