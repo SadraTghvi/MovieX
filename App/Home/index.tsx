72c208ebf5f1134a5b4a1bda54913c5a6a2d1b1e
sadra <sadrataghavi1383@gmail.com>
2022-10-22 16:38:23 +0330

    fix(home)

--- App/Home/index.tsx
+++ App/Home/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useSetAtom } from 'jotai'
+import { HeroImgAtom } from 'state'
 
 import HeroSection from './HeroSection'
 
 import './style/home.scss'
 
 const Home = () => {
+    const UpdateHeroImg = useSetAtom(HeroImgAtom)
+
+    useEffect(() => {
+        UpdateHeroImg()
+    }, [])
+
     return (
         <main className='home-container'>
             <HeroSection />
