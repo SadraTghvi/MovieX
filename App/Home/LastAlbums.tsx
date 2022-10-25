083ba3fd8693de49dcd20972db4d5e6538290ee5
sadra <sadrataghavi1383@gmail.com>
2022-10-25 23:05:52 +0330

    feat(lastalbums)

--- App/Home/LastAlbums.tsx
+++ App/Home/LastAlbums.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+import { C } from '@00-team/utils'
 
 const LastAlbums = () => {
-    return <section className='last-albums'>LastAlbums</section>
+    const container = useRef<HTMLDivElement>(null)
+    const [Transform, setTransform] = useState(0)
+
+    const UpdateTransform = () => {
+        if (!container.current) return
+        const { top } = container.current.getBoundingClientRect()
+
+        setTransform(Math.max(top, 0))
+    }
+
+    useEffect(() => {
+        UpdateTransform()
+        window.addEventListener('scroll', UpdateTransform)
+    }, [])
+
+    useEffect(() => {
+        console.log(Transform)
+    }, [Transform])
+
+    return (
+        <section ref={container} className='last-albums'>
+            <div className={`albums-title title ${C(Transform <= 600)}`}>
+                <span>Our Latest Albums</span>
+            </div>
+            <div className='albums-wrapper'></div>
+        </section>
+    )
 }
 
 export default LastAlbums
