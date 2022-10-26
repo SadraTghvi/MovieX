bee298909db1b07d670230c9c0f8e3375e45ba55
sadra <sadrataghavi1383@gmail.com>
2022-10-26 11:54:08 +0330

    feat(lastAlbums)

--- App/Home/LastAlbums.tsx
+++ App/Home/LastAlbums.tsx
@@ -2,53 +2,53 @@ import React, { FC, useEffect, useRef, useState } from 'react'
 
 import { C } from '@00-team/utils'
 
-// const SAMPLE_LAST_ALBUMS = [
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-//     {
-//         title: 'lorem ipsum',
-//         description: 'lorem ipsum the sdadw dsadw sda pusdad',
-//         img: 'https://picsum.photos/1920/1080',
-//     },
-// ]
+const SAMPLE_LAST_ALBUMS: AlbumCardProps[] = [
+    {
+        title: 'phase one',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/400/5100',
+    },
+    {
+        title: 'phase two',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/400/500',
+    },
+    {
+        title: 'phase three',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/200/300',
+    },
+    {
+        title: 'phase one',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/400/5100',
+    },
+    {
+        title: 'phase two',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/400/500',
+    },
+    {
+        title: 'phase three',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/200/300',
+    },
+    {
+        title: 'phase one',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/400/5100',
+    },
+    {
+        title: 'phase two',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/400/500',
+    },
+    {
+        title: 'phase three',
+        description: 'lorem ipsum the sdadw dsadw sda pusdad',
+        img: 'https://picsum.photos/200/300',
+    },
+]
 
 const LastAlbums: FC = () => {
     const container = useRef<HTMLDivElement>(null)
@@ -75,26 +75,55 @@ const LastAlbums: FC = () => {
         else return 0
     }
 
+    const ReturnCardData = (CardIndex: number) => {
+        if (CardIndex == 1) {
+            if (Transform < 600 && Transform >= 300)
+                return SAMPLE_LAST_ALBUMS[0]
+            else if (Transform < 300 && Transform >= 100)
+                return SAMPLE_LAST_ALBUMS[1]
+            else if (Transform < 100) return SAMPLE_LAST_ALBUMS[2]
+        }
+        if (CardIndex == 2) {
+            if (Transform < 600 && Transform >= 300)
+                return SAMPLE_LAST_ALBUMS[3]
+            else if (Transform < 300 && Transform >= 100)
+                return SAMPLE_LAST_ALBUMS[4]
+            else if (Transform < 100) return SAMPLE_LAST_ALBUMS[5]
+        }
+        if (CardIndex == 3) {
+            if (Transform < 600 && Transform >= 300)
+                return SAMPLE_LAST_ALBUMS[6]
+            else if (Transform < 300 && Transform >= 100)
+                return SAMPLE_LAST_ALBUMS[7]
+            else if (Transform < 100) return SAMPLE_LAST_ALBUMS[8]
+        }
+        return {
+            title: 'lorem ipsum',
+            description: 'lorem ipsum the sdadw dsadw sda pusdad',
+            img: 'https://picsum.photos/400/500',
+        }
+    }
+
     return (
         <section ref={container} className='last-albums'>
             <div className={`albums-title title ${C(Transform <= 600)}`}>
                 <span>Our Latest Albums</span>
             </div>
             <div className='albums-wrapper'>
+                {/* @ts-ignore */}
                 <AlbumCard
-                    title='lorem ipsum'
-                    description='lorem ipsum the sdadw dsadw sda pusdad'
                     transform={ReturnCardTransform(Transform)}
+                    {...ReturnCardData(1)}
                 />
+                {/* @ts-ignore */}
                 <AlbumCard
-                    title='lorem ipsum'
-                    description='lorem ipsum the sdadw dsadw sda pusdad'
                     transform={ReturnCardTransform(Transform)}
+                    {...ReturnCardData(2)}
                 />
+                {/* @ts-ignore */}
                 <AlbumCard
-                    title='lorem ipsum'
-                    description='lorem ipsum the sdadw dsadw sda pusdad'
                     transform={ReturnCardTransform(Transform)}
+                    {...ReturnCardData(3)}
                 />
             </div>
         </section>
@@ -104,16 +133,22 @@ const LastAlbums: FC = () => {
 interface AlbumCardProps {
     title: string
     description: string
+    img: string
     transform?: number
 }
 
-const AlbumCard: FC<AlbumCardProps> = ({ title, description, transform }) => {
+const AlbumCard: FC<AlbumCardProps> = ({
+    title,
+    description,
+    transform,
+    img,
+}) => {
     return (
         <div
             className='album-card-wrapper'
             style={{ transform: `translateY(${transform}px)` }}
         >
-            <img src='https://picsum.photos/400/500' alt='' />
+            <img src={img} alt='' />
             <div className='content-wrapper'>
                 <div className='card-title title_small'>{title}</div>
                 <div className='card-description titles_smaller'>
