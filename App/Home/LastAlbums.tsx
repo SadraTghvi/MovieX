da52c4f043aae4836033e6b2be858b9d0a43187c
sadra <sadrataghavi1383@gmail.com>
2022-10-26 15:15:39 +0330

    fix(lastalbums)

--- App/Home/LastAlbums.tsx
+++ App/Home/LastAlbums.tsx
@@ -2,55 +2,12 @@ import React, { FC, useEffect, useRef, useState } from 'react'
 
 import { C } from '@00-team/utils'
 
-const SAMPLE_LAST_ALBUMS: AlbumCardProps[] = [
-    {
-        title: 'phase one',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/400/5100',
-    },
-    {
-        title: 'phase two',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/400/500',
-    },
-    {
-        title: 'phase three',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/200/300',
-    },
-    {
-        title: 'phase one',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/400/5100',
-    },
-    {
-        title: 'phase two',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/400/500',
-    },
-    {
-        title: 'phase three',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/200/300',
-    },
-    {
-        title: 'phase one',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/400/5100',
-    },
-    {
-        title: 'phase two',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/400/500',
-    },
-    {
-        title: 'phase three',
-        description: 'lorem ipsum the sdadw dsadw sda pusdad',
-        img: 'https://picsum.photos/200/300',
-    },
-]
+import { useAtom } from 'jotai'
+import { LastAlbumAtom } from 'state'
 
 const LastAlbums: FC = () => {
+    const [LastAlbums, UpdateLastAlbums] = useAtom(LastAlbumAtom)
+
     const container = useRef<HTMLDivElement>(null)
     const [Transform, setTransform] = useState(0)
 
@@ -60,6 +17,10 @@ const LastAlbums: FC = () => {
 
         setTransform(Math.max(top, 0))
     }
+
+    useEffect(() => {
+        UpdateLastAlbums()
+    }, [])
 
     useEffect(() => {
         UpdateTransform()
@@ -77,25 +38,19 @@ const LastAlbums: FC = () => {
 
     const ReturnCardData = (CardIndex: number) => {
         if (CardIndex == 1) {
-            if (Transform < 600 && Transform >= 300)
-                return SAMPLE_LAST_ALBUMS[0]
-            else if (Transform < 300 && Transform >= 100)
-                return SAMPLE_LAST_ALBUMS[1]
-            else if (Transform < 100) return SAMPLE_LAST_ALBUMS[2]
+            if (Transform < 600 && Transform >= 300) return LastAlbums[0]
+            else if (Transform < 300 && Transform >= 100) return LastAlbums[1]
+            else if (Transform < 100) return LastAlbums[2]
         }
         if (CardIndex == 2) {
-            if (Transform < 600 && Transform >= 300)
-                return SAMPLE_LAST_ALBUMS[3]
-            else if (Transform < 300 && Transform >= 100)
-                return SAMPLE_LAST_ALBUMS[4]
-            else if (Transform < 100) return SAMPLE_LAST_ALBUMS[5]
+            if (Transform < 600 && Transform >= 300) return LastAlbums[3]
+            else if (Transform < 300 && Transform >= 100) return LastAlbums[4]
+            else if (Transform < 100) return LastAlbums[5]
         }
         if (CardIndex == 3) {
-            if (Transform < 600 && Transform >= 300)
-                return SAMPLE_LAST_ALBUMS[6]
-            else if (Transform < 300 && Transform >= 100)
-                return SAMPLE_LAST_ALBUMS[7]
-            else if (Transform < 100) return SAMPLE_LAST_ALBUMS[8]
+            if (Transform < 600 && Transform >= 300) return LastAlbums[6]
+            else if (Transform < 300 && Transform >= 100) return LastAlbums[7]
+            else if (Transform < 100) return LastAlbums[8]
         }
         return {
             title: 'lorem ipsum',
