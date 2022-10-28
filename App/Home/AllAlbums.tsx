00fb4708c3c7563af91e05d3361f2c8cafc5f57f
sadra <sadrataghavi1383@gmail.com>
2022-10-28 13:36:02 +0330

    feat(home): new section

---
+++ App/Home/AllAlbums.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AllAlbums = () => {
+    return (
+        <section className='all-albums'>
+            <header className='all-albums-title title_hero'>
+                <span>Explore Our Albums</span>
+            </header>
+            <div className='all-albums-wrapper'></div>
+        </section>
+    )
+}
+
+export default AllAlbums
