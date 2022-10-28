1850c3b93534114c25c3c8264770100b60a1eef1
sadra <sadrataghavi1383@gmail.com>
2022-10-28 18:05:36 +0330

    feat(atom)

--- App/state/atom/locale.ts
+++ App/state/atom/locale.ts
@@ -2,10 +2,11 @@ import axios from 'axios'
 
 import { atom } from 'jotai'
 
-import { HeroImg, LastAlbum } from '../models/Locale'
+import { AllAlbum, HeroImg, LastAlbum } from '../models/Locale'
 
 const HeroImgs = atom<HeroImg>([])
 const LastAlbums = atom<LastAlbum[]>([])
+const AllAlbums = atom<AllAlbum>([])
 
 const HeroImgAtom = atom(
     get => get(HeroImgs),
@@ -25,4 +26,13 @@ const LastAlbumAtom = atom(
     }
 )
 
-export { HeroImgAtom, LastAlbumAtom }
+const AllAlbumsAtom = atom(
+    get => get(AllAlbums),
+    async (_, set) => {
+        let { data } = await axios.get('http://localhost:7000/api/all-albums/')
+
+        set(AllAlbums, data.albums)
+    }
+)
+
+export { HeroImgAtom, LastAlbumAtom, AllAlbumsAtom }
