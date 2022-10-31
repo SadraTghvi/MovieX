08f36783fcb5a607c860b27285840d3fe5560a7a
sadra <sadrataghavi1383@gmail.com>
2022-10-31 13:02:58 +0330

    feat(navbar)

--- App/layout/Navbar.tsx
+++ App/layout/Navbar.tsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { C } from '@00-team/utils'
+
+import { RiMenu4Line } from '@react-icons/all-files/ri/RiMenu4Line'
+import { VscClose } from '@react-icons/all-files/vsc/VscClose'
+
+import { useAtom } from 'jotai'
+import { NavActiveAtom } from 'state'
 
 import './style/navbar.scss'
 
 const Navbar = () => {
-    const [IsActive, setIsActive] = useState(false)
+    const [IsActive, setIsActive] = useAtom(NavActiveAtom)
+
     return (
         <nav className={`navbar-container ${C(IsActive)}`}>
-            <div
-                className='toggle-nav'
-                onClick={() => setIsActive(!IsActive)}
-            ></div>
+            <div className='toggle-nav' onClick={() => setIsActive(!IsActive)}>
+                <div className={`toggle-icon ${C(IsActive)}`}>
+                    {!IsActive ? (
+                        <RiMenu4Line size={40} />
+                    ) : (
+                        <VscClose size={50} />
+                    )}
+                </div>
+            </div>
         </nav>
     )
 }
