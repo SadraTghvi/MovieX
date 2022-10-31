e0c9a00019e773fba845909a1473cadb44ee5247
sadra <sadrataghavi1383@gmail.com>
2022-10-31 13:01:51 +0330

    feat(home)

--- App/Home/index.tsx
+++ App/Home/index.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react'
 
-import { useSetAtom } from 'jotai'
-import { HeroImgAtom } from 'state'
+import { C } from '@00-team/utils'
+
+import { useAtomValue, useSetAtom } from 'jotai'
+import { HeroImgAtom, NavActiveAtom } from 'state'
 
 import AllAlbums from './AllAlbums'
 import HeroSection from './HeroSection'
@@ -11,13 +13,14 @@ import './style/home.scss'
 
 const Home = () => {
     const UpdateHeroImg = useSetAtom(HeroImgAtom)
+    const NavActive = useAtomValue(NavActiveAtom)
 
     useEffect(() => {
         UpdateHeroImg()
     }, [])
 
     return (
-        <main className='home-container'>
+        <main className={`home-container ${C(NavActive)}`}>
             <HeroSection />
             <LastAlbums />
             <AllAlbums />
