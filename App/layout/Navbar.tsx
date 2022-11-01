6bdc01472216403ba499924a3e1c66798a56f9f3
sadra <sadrataghavi1383@gmail.com>
2022-11-01 18:59:31 +0330

    feat(nav)

--- App/layout/Navbar.tsx
+++ App/layout/Navbar.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import { C } from '@00-team/utils'
 
 import { RiMenu4Line } from '@react-icons/all-files/ri/RiMenu4Line'
 import { VscClose } from '@react-icons/all-files/vsc/VscClose'
 
+import { Link } from 'react-router-dom'
+
 import { useAtom } from 'jotai'
 import { NavActiveAtom } from 'state'
 
 import './style/navbar.scss'
 
-const Navbar = () => {
+const Navbar: FC = () => {
     const [IsActive, setIsActive] = useAtom(NavActiveAtom)
 
     return (
@@ -24,8 +26,32 @@ const Navbar = () => {
                     )}
                 </div>
             </div>
-            <nav className={`navbar-container ${C(IsActive)}`}></nav>
+            <nav className={`navbar-container ${C(IsActive)}`}>
+                <NavPage
+                    img='https://picsum.photos/200/300'
+                    link='/test'
+                    title='title test'
+                />
+            </nav>
         </>
+    )
+}
+
+interface NavPageProps {
+    img: string
+    title: string
+    link: string
+}
+
+const NavPage: FC<NavPageProps> = ({ title, img, link }) => {
+    return (
+        <Link
+            style={{ backgroundImage: `url(${img})` }}
+            to={link}
+            className='nav-page'
+        >
+            <div className='title title_hero'> {title} </div>
+        </Link>
     )
 }
 
